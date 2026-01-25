22bf1fe8afcdc5239533296f7547e7e49fc32b08
Muhammad Abdullah <abdullah.muhammad.xyz@gmail.com>
2026-01-25 13:33:31 +0500

    My First Commit Made the barebone

---
+++ src/components/Todo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Todo = () => {
+  return (
+    <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-137.5 rounded-xl"></div>
+  );
+};
+
+export default Todo;
